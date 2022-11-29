50e799cdce7145ccdd9f63241b511eec5eb36e13
Dhruv Kumar <cool.dhruv225@gmail.com>
2022-11-29 22:27:34 +0530

    Data getting Successfully

--- Accelerometer.js
+++ Accelerometer.js
@@ -1,79 +1,122 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Accelerometer } from 'expo-sensors';
+import React, { useState, useEffect, useMemo } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Accelerometer, Gyroscope } from "expo-sensors";
 
 export default function App() {
-  const [{ x, y, z }, setData] = useState({
-    x: 0,
-    y: 0,
-    z: 0,
-  });
-  const [subscription, setSubscription] = useState(null);
+    const [dataArr, setData] = useState([]);
+    const [dataArr2, setData2] = useState([]);
+    const [aData, setAData] = useState({});
+    const [gData, setGData] = useState({});
+    // const [f1, setF1] = useState(0);
+    // const [f2, setF2] = useState(0);
+    const valueCount = 952;
 
-  const _slow = () => Accelerometer.setUpdateInterval(1000);
-  const _fast = () => Accelerometer.setUpdateInterval(16);
+    function startGyro() {
+        Gyroscope.setUpdateInterval(20);
+        Gyroscope.addListener((gyroData) => {
+            // arr.push([[accData.x], [accData.y], [accData.z]]);
+            // setData(arr);
+            setGData(gyroData);
+            setData2((prevData) => [
+                ...prevData,
+                [[gyroData.x], [gyroData.y], [gyroData.z]],
+            ]);
+        });
+    }
 
-  const _subscribe = () => {
-    setSubscription(
-      Accelerometer.addListener(setData)
+    useEffect(() => {
+        startGyro();
+        // return () => {
+        //     console.log("Stopped");
+        //     Gyroscope.removeAllListeners();
+        // };
+    }, []);
+
+    function startAccelerometer() {
+        Accelerometer.setUpdateInterval(20);
+        Accelerometer.addListener((accData) => {
+            // arr.push([[accData.x], [accData.y], [accData.z]]);
+            // setData(arr);
+            setAData(accData);
+            setData((prevData) => [
+                ...prevData,
+                [[accData.x], [accData.y], [accData.z]],
+            ]);
+        });
+    }
+
+    useEffect(() => {
+        startAccelerometer();
+        // return () => {
+        //     console.log("Stopped");
+        //     Accelerometer.removeAllListeners();
+        // };
+    }, []);
+
+    useMemo(() => {
+        if (dataArr.length === valueCount) {
+            Accelerometer.removeAllListeners();
+        }
+
+        if (dataArr2.length === valueCount) {
+            Gyroscope.removeAllListeners();
+        }
+        if (dataArr.length === valueCount && dataArr2.length === valueCount) {
+            let res = [];
+            for (let i = 0; i < valueCount; i++) {
+                res.push([...dataArr[i], ...dataArr2[i]]);
+            }
+            // ans.push(res);
+            console.log(res);
+        }
+    }, [dataArr, dataArr2]);
+
+    return (
+        <View style={styles.mcont}>
+            <View style={styles.container}>
+                <Text style={styles.text}>Accelerometer:</Text>
+                <Text style={styles.text}>x: {aData.x}</Text>
+                <Text style={styles.text}>y: {aData.y}</Text>
+                <Text style={styles.text}>z: {aData.z}</Text>
+            </View>
+            <View style={styles.container}>
+                <Text style={styles.text}>Gyroscope:</Text>
+                <Text style={styles.text}>x: {gData.x}</Text>
+                <Text style={styles.text}>y: {gData.y}</Text>
+                <Text style={styles.text}>z: {gData.z}</Text>
+            </View>
+        </View>
     );
-  };
-
-  const _unsubscribe = () => {
-    subscription && subscription.remove();
-    setSubscription(null);
-  };
-
-  useEffect(() => {
-    _subscribe();
-    return () => _unsubscribe();
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Accelerometer: (in gs where 1g = 9.81 m/s^2)</Text>
-      <Text style={styles.text}>x: {x}</Text>
-      <Text style={styles.text}>y: {y}</Text>
-      <Text style={styles.text}>z: {z}</Text>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
-          <Text>{subscription ? 'On' : 'Off'}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
-          <Text>Slow</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={_fast} style={styles.button}>
-          <Text>Fast</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingHorizontal: 20,
-  },
-  text: {
-    textAlign: 'center',
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    alignItems: 'stretch',
-    marginTop: 15,
-  },
-  button: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#eee',
-    padding: 10,
-  },
-  middleButton: {
-    borderLeftWidth: 1,
-    borderRightWidth: 1,
-    borderColor: '#ccc',
-  },
+    mcont: {
+        marginTop: 200,
+    },
+
+    container: {
+        marginTop: 50,
+        justifyContent: "center",
+        paddingHorizontal: 20,
+    },
+    text: {
+        textAlign: "center",
+    },
+    buttonContainer: {
+        flexDirection: "row",
+        alignItems: "stretch",
+        marginTop: 15,
+    },
+    button: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#eee",
+        padding: 10,
+    },
+    middleButton: {
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+        borderColor: "#ccc",
+    },
 });
